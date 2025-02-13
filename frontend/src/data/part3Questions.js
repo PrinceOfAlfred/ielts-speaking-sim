e9b567c7911e895d7610153ebe64a6aa1fd965b4
Fungai Nkosana Pfende <fungaip163@outlook.com>
2025-02-13 14:27:45 +0200

    Revert "initialize frontend and backend structure with basic components and"
    This reverts commit 71ebef239567d5bc512b1dba01cf8a56497f87f7.

--- frontend/src/data/part3Questions.js
+++
@@ -1,5 +0,0 @@
-export const part3Questions = [
-  "What kind of things give status to people in your country?",
-  "Have things changed since your parents’ time?",
-  "Do you think advertising influences what people buy?"
-];
